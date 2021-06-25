0112d908b91f3e487e7c367061fac8419c3cde08
SEBBAR <76476475+Sebbar-Najlae@users.noreply.github.com>
2021-06-25 22:01:46 +0100

    From csv to db

---
+++ import-csv/import-csv/index.php
@@ -0,0 +1,217 @@
+<?php
+use Phppot\DataSource;
+
+require_once 'DataSource.php';
+$db = new DataSource();
+$conn = $db->getConnection();
+
+if (isset($_POST["import"])) {
+    
+    $fileName = $_FILES["file"]["tmp_name"];
+    
+    if ($_FILES["file"]["size"] > 0) {
+        
+        $file = fopen($fileName, "r");
+        
+        while (($column = fgetcsv($file, 10000, ",")) !== FALSE) {
+            
+            $userId = "";
+            if (isset($column[0])) {
+                $userId = mysqli_real_escape_string($conn, $column[0]);
+            }
+            $userName = "";
+            if (isset($column[1])) {
+                $userName = mysqli_real_escape_string($conn, $column[1]);
+            }
+            $password = "";
+            if (isset($column[2])) {
+                $password = mysqli_real_escape_string($conn, $column[2]);
+            }
+            $firstName = "";
+            if (isset($column[3])) {
+                $firstName = mysqli_real_escape_string($conn, $column[3]);
+            }
+            $lastName = "";
+            if (isset($column[4])) {
+                $lastName = mysqli_real_escape_string($conn, $column[4]);
+            }
+            
+            $sqlInsert = "INSERT into users (userId,userName,password,firstName,lastName)
+                   values (?,?,?,?,?)";
+            $paramType = "issss";
+            $paramArray = array(
+                $userId,
+                $userName,
+                $password,
+                $firstName,
+                $lastName
+            );
+            $insertId = $db->insert($sqlInsert, $paramType, $paramArray);
+            
+            if (! empty($insertId)) {
+                $type = "success";
+                $message = "CSV Data Imported into the Database";
+            } else {
+                $type = "error";
+                $message = "Problem in Importing CSV Data";
+            }
+        }
+    }
+}
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+<script src="jquery-3.2.1.min.js"></script>
+
+<style>
+body {
+    font-family: Arial;
+    width: 550px;
+}
+
+.outer-scontainer {
+    background: #F0F0F0;
+    border: #e0dfdf 1px solid;
+    padding: 20px;
+    border-radius: 2px;
+}
+
+.input-row {
+    margin-top: 0px;
+    margin-bottom: 20px;
+}
+
+.btn-submit {
+    background: #333;
+    border: #1d1d1d 1px solid;
+    color: #f0f0f0;
+    font-size: 0.9em;
+    width: 100px;
+    border-radius: 2px;
+    cursor: pointer;
+}
+
+.outer-scontainer table {
+    border-collapse: collapse;
+    width: 100%;
+}
+
+.outer-scontainer th {
+    border: 1px solid #dddddd;
+    padding: 8px;
+    text-align: left;
+}
+
+.outer-scontainer td {
+    border: 1px solid #dddddd;
+    padding: 8px;
+    text-align: left;
+}
+
+#response {
+    padding: 10px;
+    margin-bottom: 10px;
+    border-radius: 2px;
+    display: none;
+}
+
+.success {
+    background: #c7efd9;
+    border: #bbe2cd 1px solid;
+}
+
+.error {
+    background: #fbcfcf;
+    border: #f3c6c7 1px solid;
+}
+
+div#response.display-block {
+    display: block;
+}
+</style>
+<script type="text/javascript">
+$(document).ready(function() {
+    $("#frmCSVImport").on("submit", function () {
+
+	    $("#response").attr("class", "");
+        $("#response").html("");
+        var fileType = ".csv";
+        var regex = new RegExp("([a-zA-Z0-9\s_\\.\-:])+(" + fileType + ")$");
+        if (!regex.test($("#file").val().toLowerCase())) {
+        	    $("#response").addClass("error");
+        	    $("#response").addClass("display-block");
+            $("#response").html("Invalid File. Upload : <b>" + fileType + "</b> Files.");
+            return false;
+        }
+        return true;
+    });
+});
+</script>
+</head>
+
+<body>
+    <h2>Import CSV file into Mysql using PHP</h2>
+
+    <div id="response"
+        class="<?php if(!empty($type)) { echo $type . " display-block"; } ?>">
+        <?php if(!empty($message)) { echo $message; } ?>
+        </div>
+    <div class="outer-scontainer">
+        <div class="row">
+
+            <form class="form-horizontal" action="" method="post"
+                name="frmCSVImport" id="frmCSVImport"
+                enctype="multipart/form-data">
+                <div class="input-row">
+                    <label class="col-md-4 control-label">Choose CSV
+                        File</label> <input type="file" name="file"
+                        id="file" accept=".csv">
+                    <button type="submit" id="submit" name="import"
+                        class="btn-submit">Import</button>
+                    <br />
+
+                </div>
+
+            </form>
+
+        </div>
+               <?php
+            $sqlSelect = "SELECT * FROM users";
+            $result = $db->select($sqlSelect);
+            if (! empty($result)) {
+                ?>
+            <table id='userTable'>
+            <thead>
+                <tr>
+                    <th>User ID</th>
+                    <th>User Name</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+
+                </tr>
+            </thead>
+<?php
+                
+                foreach ($result as $row) {
+                    ?>
+                    
+                <tbody>
+                <tr>
+                    <td><?php  echo $row['userId']; ?></td>
+                    <td><?php  echo $row['userName']; ?></td>
+                    <td><?php  echo $row['firstName']; ?></td>
+                    <td><?php  echo $row['lastName']; ?></td>
+                </tr>
+                    <?php
+                }
+                ?>
+                </tbody>
+        </table>
+        <?php } ?>
+    </div>
+
+</body>
+
+</html>
