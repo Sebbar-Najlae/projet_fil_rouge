f3cd311ab6d63f8d3f9efb7712a1d2c970854e75
sebbar najlae <76476475+Sebbar-Najlae@users.noreply.github.com>
2021-07-07 13:22:31 +0100

    class connexion

---
+++ connexion/conn/pages/indexdash.php
@@ -0,0 +1,34 @@
+<?php
+	session_start();
+	
+	
+	}
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<META HTTP-EQUIV=Refresh; 
+	<head>
+	<link rel="stylesheet" href="../style/Style.css">
+		<title>CONNECTED</title>
+		
+	
+	</head>
+	<body>
+	<h2>Welcome <span style="color:green"><?php
+	echo $_SESSION['UserName'];
+	?></span></h2>
+	
+	
+	
+		
+	
+	
+	</body>
+</html>
+
+
+		
+	
+	
+	
+
